dacd1487393f49103c936622f597e6ed93fb03d4
markgraves <mgraves@xweave.com>
2018-05-01 09:42:46 -0400

    updated for python 3

--- salan/normword.py
+++ salan/normword.py
@@ -1,43 +1,52 @@
-#!/usr/bin/python
-from __future__ import division, print_function
-import nltk
+"""Normalize word"""
+
 import logging
+import nltk
 
-logger = logging.getLogger('salan')
-log_filename = 'log.txt'
+LOGGER = logging.getLogger(__name__)
 
-word_pos = {}
-#Note: initializing normalize_word_cache supports the creation of wordsets for a collection of words
-normalize_word_cache = {}
+NORMWORD_WNL = None
+NORMWORD_POS = {}
+NORMWORD_CACHE = {}
 
-normalize_word_wnl = nltk.stem.wordnet.WordNetLemmatizer()
+
+def init_normword_wnl():
+    "Initialize word net lemmatizer for normword"
+    global NORMWORD_WNL
+    NORMWORD_WNL = nltk.stem.wordnet.WordNetLemmatizer()
+    return NORMWORD_WNL
+
+
 def normalize_word(word, lowercase=True, lemmatize=True):
-    global normalize_word_cache
-    global word_pos
+    "Normalize word by stripping plural nouns"
+    global NORMWORD_CACHE
+    global NORMWORD_POS
+    if NORMWORD_WNL is None:
+        init_normword_wnl()
     if lowercase:
-        word_lower = word.lower()
-    else:
-        word_lower = word # needed for segment_sentence
-    if word_lower in normalize_word_cache:
-        return normalize_word_cache[word_lower]
-    newword = word_lower
+        word = word.lower()
+    if word in NORMWORD_CACHE:
+        return NORMWORD_CACHE[word]
     if not lemmatize:
-        return newword
+        return word
     treebank_tag = nltk.pos_tag([word])[0][1]
+    newword = word
     if ( len(newword) > 4 ) and ( treebank_tag == 'NNS' ):
-        # Only lemmatize plural nouns, leave verbs alone
+        #  Only lemmatize plural nouns, leave verbs alone
         wnpos = get_wordnet_pos(treebank_tag)
         if wnpos:
-            newword = normalize_word_wnl.lemmatize(newword, wnpos)
-        #if newword != word_lower:
-            #print('Changing %s to %s' % (word_lower, newword))
-        word_pos[newword] = nltk.pos_tag([newword])[0][1]
+            newword = NORMWORD_WNL.lemmatize(newword, wnpos)
+        if newword != word:
+            LOGGER.debug('Changing %s to %s' % (word, newword))
+        NORMWORD_POS[newword] = nltk.pos_tag([newword])[0][1]
     else:
-        word_pos[word_lower] = treebank_tag
-    normalize_word_cache[word_lower] = newword
+        NORMWORD_POS[word] = treebank_tag
+    NORMWORD_CACHE[word] = newword
     return newword
 
+
 def get_wordnet_pos(treebank_tag):
+    "Translate from treebank pos tags to wordnet pos tags."
     key = treebank_tag[0:1]
     if key =='N':
         return nltk.corpus.wordnet.NOUN
